83e1837fcb3df55fd0f846ee233e056c5b73a4d9
julianandrei11 <sagonjulian@gmail.com>
2025-06-25 09:10:00 +0800

    crude for goals

--- src/app/goals/goals.page.ts
+++ src/app/goals/goals.page.ts
@@ -4,13 +4,58 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-goals',
   templateUrl: './goals.page.html',
   styleUrls: ['./goals.page.scss'],
-  standalone:false
+  standalone: false
 })
 export class GoalsPage implements OnInit {
 
-  constructor() { }
+  goalLabel: string = '';
+  goalValue: number = 1000;
+  goals: { label: string; value: number }[] = [];
 
-  ngOnInit() {
+  isEditing: boolean = false;
+  editingIndex: number | null = null;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  addGoal() {
+    if (!this.goalLabel || !this.goalValue) return;
+
+    if (this.isEditing && this.editingIndex !== null) {
+      // Update existing goal
+      this.goals[this.editingIndex] = {
+        label: this.goalLabel,
+        value: this.goalValue
+      };
+      this.cancelEdit();
+    } else {
+      // Add new goal
+      this.goals.push({
+        label: this.goalLabel,
+        value: this.goalValue
+      });
+    }
+
+    this.goalLabel = '';
   }
 
+  editGoal(index: number) {
+    this.goalLabel = this.goals[index].label;
+    this.goalValue = this.goals[index].value;
+    this.isEditing = true;
+    this.editingIndex = index;
+  }
+
+  deleteGoal(index: number) {
+    this.goals.splice(index, 1);
+    this.cancelEdit(); // cancel edit if item is deleted while editing
+  }
+
+  cancelEdit() {
+    this.isEditing = false;
+    this.editingIndex = null;
+    this.goalLabel = '';
+    this.goalValue = 1000;
+  }
 }
